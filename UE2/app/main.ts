4f4baf50edcf5e1ffe4214199d334cde3a841bc9
porszivo <marcel.k@just-click.org>
2019-02-18 21:05:11 +0100

    Projekt restrukturiert

--- UE2/app/main.ts
+++
@@ -1,7 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
-
-/**
- * Entry point to App, must not be changed
- */
-platformBrowserDynamic().bootstrapModule(AppModule);
--- UE4/Client/src/app/model/controlType.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Typen der Steuerungselemente
- */
-export enum ControlType {
-  boolean = 0,
-  enum = 1,
-  continuous = 2
-}
